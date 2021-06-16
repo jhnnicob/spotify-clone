249c1ffbd878f184e384e2720ab4642b8f69d2a5
Nico Molon <johnnicomolon@yahoo.com>
2021-06-16 19:46:27 +0800

    Added styles

---
+++ src/Body.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Body.css';
+
+export default function Body() {
+    return(
+        <div className="body">
+            <h1>I am the Body</h1>
+        </div>
+    )
+}
