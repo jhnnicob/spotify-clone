9578ccca27b9c3c9e1a89582a6541cc3b4b8f6b8
Nico Molon <johnnicomolon@yahoo.com>
2021-06-16 19:43:59 +0800

    Update sidebaroption

---
+++ src/SidebarOption.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './SidebarOption.css'
+
+export default function SidebarOption({title, Icon}) {
+    return <div className="sidebarOption">
+        {Icon && <Icon className="sidebarOption_icon" />}
+        {Icon ? <h4>{title}</h4> : <p>{title}</p>}
+    </div>
+}
