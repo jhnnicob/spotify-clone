7a429cfb52de7f0f80e43cfbbbc7dbdf8d92991c
Nico Molon <johnnicomolon@yahoo.com>
2021-06-16 19:45:23 +0800

    Update Player

--- src/Player.js
+++ src/Player.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import './Player.css'
+import Sidebar from './Sidebar'
+import Body from './Body'
+import Footer from './Footer'
 
-export default function Player() {
+export default function Player({player}) {
 
     return(
-        <h1>Welcome to Spotify</h1>
+        <div className="player">
+            <div className="player_body">
+                <Sidebar />
+                <Body />
+            </div>
+            <Footer />
+        </div>
     )
 }
