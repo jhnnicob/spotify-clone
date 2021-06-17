7949010ca18b31aa98b8291b2b2adddb202533c7
Nico Molon <johnnicomolon@yahoo.com>
2021-06-17 16:20:48 +0800

    Added footer component and Styles

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,48 @@
 import React from 'react'
 import './Footer.css'
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+
+import { Grid, Slider } from '@material-ui/core';
 
 export default function Footer() {
     return(
         <div className="footer">
-            <h1>I am the footer</h1>
+            <div className="footer_left">
+                <img 
+                    className="footer_albumn_logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/4/45/Usher.jpg" 
+                    alt="" />
+                <div className="footer_songInfo">
+                    <h4>Yeah</h4>
+                    <p>Usher</p>
+                </div>
+            </div>
+            <div className="footer_center">
+                <ShuffleIcon className="footer_green" />
+                <SkipPreviousIcon className="footer_icon" />
+                <PlayCircleOutlineIcon fontSize="large" className="footer_icon" />
+                <SkipNextIcon className="footer_icon" />
+                <RepeatIcon className="footer_green" />
+            </div>
+            <div className="footer_right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider />
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     )
 }
