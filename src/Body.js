72fdb03c3733fa98d822f89ecaabfd8633895a21
Nico Molon <johnnicomolon@yahoo.com>
2021-06-17 16:21:31 +0800

    added body components & styles

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import './Body.css';
+import { useDataLayerValue } from './DataLayer';
+import Header from './Header';
+import SongRow from './SongRow';
 
-export default function Body() {
+export default function Body({spotify}) {
+
+    const [{discover_weekly}, dispatch] = useDataLayerValue();
+    
     return(
         <div className="body">
-            <h1>I am the Body</h1>
+            <Header spotify={spotify}/>
+
+            <div className="body_info">
+                <img src={discover_weekly?.images[0].url} alt="" />
+                <div className="body_infoText">
+                    <strong>PLAYLISTS</strong>
+                    <h2>Discover Weekly</h2>
+                    <p>{discover_weekly?.description}</p>
+                </div>
+            </div>
+
+            <div className="body_songs">
+                <div className="body_icons">
+                    <PlayCircleFilledIcon className="body_shuffle"/>
+                    <FavoriteIcon />
+                    <MoreHorizIcon />
+                </div>
+                {/**List of songs */}
+                {discover_weekly?.tracks.items.map(item => (
+                    <SongRow track={item.track} />
+                ))}
+            </div>
         </div>
     )
 }
